e37a3f4de4421279798a6f02b3ff0097aabad11b
abdelrahman mousa <abdelrahmanmysara33@gmail.com>
2023-05-10 14:03:11 +0200

    feat: added redemption testing

--- test/voucher/redeem-voucher.spec.ts
+++ test/voucher/redeem-voucher.spec.ts
@@ -27,4 +27,24 @@ describe('redeem customer voucher', () => {
     expect(res.body.statusCode).toBe(400);
     expect(res.body.message[0]).toContain('voucher code is invalid');
   });
+
+  it("shouldn't be able to redeem twice", async () => {
+    const voucher = await voucherFactory();
+    const customer = await customerTestRepo().find(voucher.customerId);
+    const res = await testRequest({
+      method: HTTP_METHODS_ENUM.PATCH,
+      url: VOUCHERS,
+      variables: { email: customer.email, code: voucher.code },
+    });
+    expect(+res.text).toBeGreaterThanOrEqual(1);
+    const secondRedemption = await testRequest({
+      method: HTTP_METHODS_ENUM.PATCH,
+      url: VOUCHERS,
+      variables: { email: customer.email, code: voucher.code },
+    });
+    expect(secondRedemption.body.statusCode).toBe(400);
+    expect(secondRedemption.body.message[0]).toContain(
+      'voucher code is invalid',
+    );
+  });
 });
